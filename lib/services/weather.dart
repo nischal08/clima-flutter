70fefb9e4375f4f30a454b0f4e9c7c475dfa8e2d
nischal08 <nischal.karanjit@gmail.com>
2020-11-09 17:59:21 +0545

    Refactoring the location method

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,22 @@
+import 'package:clima/services/location.dart';
+
+import 'networking.dart';
+
+const apiKey = 'ae40737123781528d4e21a164ca1d60b';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
